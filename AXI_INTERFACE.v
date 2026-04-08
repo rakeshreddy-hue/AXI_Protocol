b272e369a6d63704ecae798970912a4b2917e70c
rakeshreddy-hue <dudekondar.ug23.ec@nitp.ac.in>
2026-04-08 10:54:41 +0530

    Update AXI_INTERFACE.v

--- AXI_INTERFACE.v
+++ AXI_INTERFACE.v
@@ -31,7 +31,104 @@ module axi_top_ram#(parameter data_width = 32,
   input [addr_width -1 : 0] s_axi_araddr,
   input s_axi_arvalid,
   output reg s_axi_arready);
-  
+
+  localparam resp_okay = 2'b00:
+  localparam resp_slv_err = 2'b01;
+
+  //state machine parameters
+
+  localparam idle = 2'b00;
+  localparam write = 2'b01;
+  localparam read = 2'b10;
+  localparam resp = 2'b11;
+
+  //internal signals
+
+  reg [1:0] write_state, read_state;
+  reg [addr_width -1:0] ram_addr;
+  reg [addr_width -1:0] ram_write_data;
+  wire  [addr_width -1:0] ram_rdata;
+
+  //instantiate ram design 
+
+  ram_design dut(aclk,aresetn,ram_write_enable,ram_addr, ram_write_enable, ram_rdata);
+
+  //write_address_channel
+  always@(posedge clk)
+    begin
+      if(!aresetn)
+        begin
+          s_axi_awready<=1'b0;
+          write_addr_reg <= {addr_width{1'b0}};
+          write_state <= idle;
+        end
+      else
+        case(write_state)
+
+          idle : begin
+            s_axi_awready<=1'b1;
+            (s_axi_awvalid && a_axi_awready) begin
+             write_addr_reg <= s_axi_awaddr;
+              s_axi_awready <= 0;
+
+              write_state <= write;
+
+            end
+          end
+
+          write : begin
+            if(s_axi_wvalid && s_axi_wready) begin
+              write_state <= resp;
+            end
+
+            resp : begin
+              if(s_axi_bvalid && s_axi_bready) begin
+                write_state <= idle;
+              end
+
+              default : write_stae <= idle;
+            end
+          end
+
+          // write data channel 
+
+          always@(posedge clk)
+            if(!aresetn)
+              begin
+                s_axi_wready <= 1'b0;
+                ram_write_enable <= 1'b0;
+                ram_write_data <= {addr_width{1'b0}};
+              end
+          else
+            if(write_state == write)begin
+              s_axi_wready <= 1'b1;
+              ram_addr <= write_addr_reg;
+              ram_write_data <= s_axi_wdata;
+              s_axi_wready <= 1'b0;
+            end
+          else
+               s_axi_wready <= 1'b0;
+          end
+          
+    
+    // write response channel 
+
+          always@(posedge clk)
+            if(!aresetn)
+              begin
+                s_axi_bvalid <= 1'b0;
+                s_axi_bresp <= resp_okay;
+              end
+          else
+            if(write_state == resp)begin
+              s_axi_bvalid <= 1'b1;
+             
+              ram_write_data <= s_axi_wdata;
+              s_axi_wready <= 1'b0;
+            end
+          else
+               s_axi_wready <= 1'b0;
+          end
 
   
                     
